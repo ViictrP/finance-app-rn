3d4a29cdb976c2eccdb141f238d04e00095d2b32
Victor Prado <vpradodev@gmail.com>
2020-05-22 19:18:44 -0300

    Melhorias no layout

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -13,26 +13,25 @@ export default function TransactionItem({title, description, icon, value, when})
 					<View style={{
 						alignItems: 'center',
 						justifyContent: 'center',
-						width: 48,
-						height: 48,
 						marginRight: 20,
 						borderRadius: 10
 					}}>
 						<Icon name={icon} size={28} color={Colors.FADDED_TEXT}/>
 					</View>
-					<View style={{flexDirection: 'column', justifyContent: 'space-between', alignContent: 'flex-start'}}>
-						<ProductSansText style={{fontSize: 20, color: Colors.TEXT_PRIMARY}}>
+					<View style={{flexDirection: 'column', alignContent: 'flex-start'}}>
+						<ProductSansText style={{fontSize: 15, marginBottom: 5, color: Colors.FADDED_TEXT}}>
+							Transporte
+						</ProductSansText>
+						<ProductSansText style={{fontSize: 20, marginBottom: 7, color: Colors.TEXT_PRIMARY}}>
 							{title}
 						</ProductSansText>
-						<ProductSansText style={{fontSize: 15, color: Colors.FADDED_TEXT}}>
-							{description}
+						<ProductSansText style={{fontSize: 18}}>
+							R$&nbsp;{value}
 						</ProductSansText>
 					</View>
 				</View>
 				<View style={{flexDirection: 'column', justifyContent: 'space-between', alignItems: 'flex-end'}}>
-					<ProductSansText style={{fontSize: 20, color: Colors.TEXT_ACCENT}}>
-						-R${value}
-					</ProductSansText>
+
 					<ProductSansText style={{color: Colors.FADDED_TEXT}}>
 						{when}
 					</ProductSansText>
